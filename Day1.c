fcd6483b76788ecd716aea8bf1e6b40943c40f42
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-01-23 00:16:02 +0800

    Create Day1.c

---
+++ Day1.c
@@ -0,0 +1,15 @@
+//1.写出主函数（main函数）
+//C语言是从主函数的第一行开始执行的
+//所以C语言代码中得有主函数-入口
+//int 是整型
+//printf - 库函数 - 在屏幕上打印信息
+//printf的使用，引用头文件(stdio.h)
+
+#include <stdio.h>
+int main() {
+	printf("hello world");
+		return 0;
+}
+
+//编译+链接+运行
+//快捷键：ctrl + f5
