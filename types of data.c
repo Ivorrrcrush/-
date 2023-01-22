decc91f0c39111db2134a2718d5f410f17e6423d
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-01-23 01:38:34 +0800

    Update types of data.c

--- types of data.c
+++ types of data.c
@@ -1,17 +1,18 @@
 //数据类型
-char      //字符数据类型
-short     //短整型
-int       //整型
-long      //长整型
-long long //更长的整型
-float     //单精度浮点数
-double    //双精度浮点数
+//char      //字符数据类型
+//short     //短整型
+//int       //整型
+//long      //长整型
+//long long //更长的整型
+//float     //单精度浮点数
+//double    //双精度浮点数
+#include <stdio.h>
 char a = 'a';
 int age = 20;
 short num = 10;
 float weight = 55.5;
 double d = 0.0;
-
+int main(){
 	//printf("hehe\n");
 	//printf("%d\n",100);  //打印一个整数 - %d
 
@@ -23,3 +24,4 @@ double d = 0.0;
 	printf("%d\n", sizeof(long long));// 8
 	printf("%d\n", sizeof(float));    // 4
 	printf("%d\n", sizeof(double));   // 8
+}	
