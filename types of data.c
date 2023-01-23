d2604cd1b42f3054daf577874c0a72d7a46023c3
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-01-23 11:13:33 +0800

    Update types of data.c

--- types of data.c
+++ types of data.c
@@ -21,8 +21,17 @@ int main(){
 	printf("%d\n", sizeof(char));     // 1
 	printf("%d\n", sizeof(short));    // 2
 	printf("%d\n", sizeof(int));      // 4
-	printf("%d\n", sizeof(long));     // 4
+	printf("%d\n", sizeof(long));     // 4  sizepf(long)>= sizeof(int)
 	printf("%d\n", sizeof(long long));// 8
 	printf("%d\n", sizeof(float));    // 4
 	printf("%d\n", sizeof(double));   // 8
 }	
+
+//计算机中的单位:
+//bit - 比特位
+//byte - 字节 = 8 bit
+//kb = 1024 byte
+//mb = 1024 kb
+//gb = 1024 mb
+//tb = 1024 gb
+//pb = 1024 tb
