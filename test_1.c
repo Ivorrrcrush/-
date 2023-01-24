f1403c17cd86a4ca762f858d9decd111e07407ad
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-01-24 15:44:48 +0800

    Update test_1.c

--- test_1.c
+++ test_1.c
@@ -1,26 +1,51 @@
 #define _CRT_SECURE_NO_WARNINGS 1;
 #include <stdio.h>
-//求十个数中最大值
-int main() {
 
+//十个数中找最大值
+//int main() {
+//
+//	//数组
+//	int arr[10] = { 0 };
+//
+//	//输入
+//	int i = 0;
+//	for (i = 0; i < 10; i++) {
+//		scanf("%d\n", &arr[i]);
+//	}
+//
+//	//找最大值
+//	int max = 0;
+//	for (i = 0; i <= 9; i++) {
+//		if (max < arr[i]) {
+//			max = arr[i];
+//		}
+//	}
+//	//输出
+//	printf("max = %d\n", max);
+//
+//	return 0;
+//}
+
+//十个整数的平均值
+int main() {
 	//数组
 	int arr[10] = { 0 };
-
 	//输入
 	int i = 0;
+	int sum = 0;
 	for (i = 0; i < 10; i++) {
-		scanf("%d\n", &arr[i]);
+		scanf("%d", &arr[i]);
+		sum += arr[i];
 	}
-
-	//找最大值
-	int max = 0;
-	for (i = 0; i <= 9; i++) {
-		if (max < arr[i]) {
-			max = arr[i];
-		}
-	}
+	//求和
+	//int sum = 0;
+	//for (i = 0; i <= 9; i++) {
+	//	sum += arr[i];
+	//}
+	//求平均数
+	int a = sum / 10;
 	//输出
-	printf("max = %d\n", max);
-
+	printf("a = %d\n", a);
+	
 	return 0;
 }
