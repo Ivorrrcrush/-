b332c12d5b6892397421ddfa52d4143a3a9fd281
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-02-02 19:49:29 +0800

    Update and rename 。。。 to ifElse.c

---
+++ ifElse.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1;
+#include <stdio.h>
+
+int main() {
+	printf("自学成才：）\n");
+	printf("要好好学习吗？：）\n");
+	int a;
+	scanf("%d", &a);
+	if (a == 1)
+		printf("上天");
+	else
+		printf("搬砖");
+	return 0;
+}
