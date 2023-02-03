b2c8c9e92106ba1030ef2ee66c082642a18c1229
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-02-03 21:24:43 +0800

    Create actor.c

---
+++ actor.c
@@ -0,0 +1,46 @@
+//int main() {
+//	//int a = 9 / 2;
+//	//float a = 9.0 / 2;
+//	int a = 9 % 2;
+//	printf("%d", a);
+//	return 0;
+//}
+
+//int main() {
+//	int a = 2;
+//	int b = a << 1;
+//	//左移运算符 - 移动的是二进制位
+//	printf("%d\n", b);
+//	return 0;
+//}
+
+//int main() {
+//	//0 表示假，非零就是真
+//	int a = 10;
+//	printf("%d\n", !a);
+//	if (a) {
+//		....
+//	}
+//	if (!a) {
+//		....
+//	}
+//	return 0;
+//}
+
+//int main() {
+//	//sizeof 是一个操作符
+//	//不是函数
+//	//计算类型或者变量的大小
+//	int a = 10;
+//	printf("%d\n", sizeof(int));
+//	printf("%d\n", sizeof a);
+
+//int arr[10] = { 0 };
+//printf("%d\n", sizeof(arr)); //计算的是数组的总大小，单位是字节
+//printf("%d\n", sizeof(arr[0]));
+//int sz = sizeof(arr) / sizeof(arr[0]);
+//printf("%d\n", sz);
+//	return 0;
+//}
+
+
