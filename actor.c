3f92db6f83159814ded71595e2621d679d88a804
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-02-05 20:46:07 +0800

    Update actor.c

--- actor.c
+++ actor.c
@@ -79,3 +79,57 @@
 //	printf("%d\n", a);
 //	return 0;
 //}
+
+//int main(){
+//	int a = 0;
+//	int b = 0;
+//	int c = a || b;
+//	printf("%d\n", c);
+//	//int a = 3;
+//	//int b = 5;
+//	//int c = a && b;
+//	//printf("%d\n", c);
+//	return 0;
+//}
+
+//int main() {
+//	int a = 0;
+//	int b = 3;
+//
+//	//if (a > b) {
+//	//	printf("%d\n", a);
+//	//}
+//	//else {
+//	//	printf("%d\n", b);
+//	//}
+//	int max = a > b ? a : b;
+//	printf("%d\n", max);
+//	return 0;
+//}
+
+//int main() {
+//	int a = 0;
+//	int b = 3;
+//	int c = 5;
+//	int d = (a = b + 2, c = a - 4, b = c + 2);
+//
+//	//逗号表达式，从左向右计算
+//	//整个表达式的结果是最后一个表达式的结果
+//
+//	printf("%d\n", d);
+//	return 0;
+//}
+
+//int main() {
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n", arr[5]);
+//	//[] - 下标引用操作符
+//	return 0;
+//}
+
+//int main() {
+//	//调用函数的时候，函数后面的()就是函数调用操作符
+//	printf("hehe");
+//	printf("%d", 100);
+//	return 0;
+//}
