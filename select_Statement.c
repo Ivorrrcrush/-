a9b754ed6847992f0fe304a0e2029a85effde8b0
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-02-11 21:06:50 +0800

    Update select_Statement.c

--- select_Statement.c
+++ select_Statement.c
@@ -1,14 +1,43 @@
-#define _CRT_SECURE_NO_WARNINGS 1;
-#include <stdio.h>
+//#define _CRT_SECURE_NO_WARNINGS 1;
+//#include <stdio.h>
+//
+//int main() {
+//	printf("自学成才：）\n");
+//	printf("要好好学习吗？：）\n");
+//	int a;
+//	scanf("%d", &a);
+//	if (a == 1)
+//		printf("上天");
+//	else
+//		printf("搬砖");
+//	return 0;
+//}
 
-int main() {
-	printf("自学成才：）\n");
-	printf("要好好学习吗？：）\n");
-	int a;
-	scanf("%d", &a);
-	if (a == 1)
-		printf("上天");
-	else
-		printf("搬砖");
-	return 0;
-}
+/*int main() {
+
+	int age = 60;
+
+	if (age < 18) {
+		printf("少年\n");
+	}else if (18 <= age && age < 26) {
+		printf("青年\n");
+	}else if ("age>=2 6&& age<40") {
+		printf("中年\n");
+	}else if (age >= 40 && age < 60) {
+		printf("壮年\n");
+	}else if (age >= 60 & age < 100) {
+		printf("老年\n");
+	}else {
+		printf("老不死\n");
+	}*/
+
+	/*if (age >= 18) {
+		printf("成年\n");
+	}else {
+		printf("未成年\n");
+		printf("不能谈恋爱\n");
+	}*/
+
+	/*if (age >= 18) {
+		printf("%s\n", "成年");
+	}*/
