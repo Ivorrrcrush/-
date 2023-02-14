5bc8065a71debf07dcd789e5d04c0b7e55bc6163
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-02-14 20:39:44 +0800

    Update switch_select_Statement.c

--- switch_select_Statement.c
+++ switch_select_Statement.c
@@ -1,28 +1,48 @@
-int main() {
+//int main() {
 //	int day;
 //	scanf("%d", &day);
 //	switch (day) {
 //	case 1:
-//		printf("Monday\n");
-//		break;
 //	case 2:
-//		printf("Tuesday\n");
-//		break;
 //	case 3:
-//		printf("Wednesday\n");
-//		break;
 //	case 4:
-//		printf("Thursday\n");
-//		break;
 //	case 5:
-//		printf("Friday\n");
+//		printf("工作日\n");
 //		break;
 //	case 6:
-//		printf("Saturday\n");
-//		break;
 //	case 7:
-//		printf("Sunday\n");
+//		printf("休息日\n");
+//		break;
+//	default:
+//		printf("输入错误\n");
 //		break;
 //	}
-//
+//	
 //}
+
+
+int main() {
+	int m = 2;
+	int n = 1;
+	switch (n) {
+	case 1:
+		m++;
+	case 2:
+		n++;
+	case 3:
+		switch (n) {
+		case 1:
+			n++;
+		case 2:
+			m++;
+			n++;
+			break;
+		}
+	case 4:
+		m++;
+	default:
+		break;
+	}
+	printf("m = %d,n = %d", m, n);
+	return 0;
+}
