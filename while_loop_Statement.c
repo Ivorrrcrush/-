0b833e49ac4f9c4bce07b1b7d04bfbdc361375a0
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-02-14 20:46:42 +0800

    Update and rename loop_Statement.c to while_loop_Statement.c

---
+++ while_loop_Statement.c
@@ -0,0 +1,24 @@
+//int main() {
+//	// 爱你三千年
+//
+//	int year = 0;
+//	//循环
+//	while (year < 3000) {
+//		printf("爱你 — %d\n", year);
+//		year++;
+//	}
+//	if (year == 3000); {
+//		printf("下一位");
+//	}
+//	return 0;
+//}
+
+
+//int main() {
+//	int i = 1;
+//	while (i <= 10) {
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
