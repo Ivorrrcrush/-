7a75fe5d5001cce78164bbda3d78bb1e6109b9e1
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-02-15 21:26:01 +0800

    Update while_loop_Statement.c

--- while_loop_Statement.c
+++ while_loop_Statement.c
@@ -15,10 +15,39 @@
 
 
 //int main() {
+//	//在while循环中，break用于永久的中止循环
+//	//在while循环中，continue用于跳过本次循环continue后面的代码，直接去判断部分，看是否进行下一次循环
 //	int i = 1;
+//
 //	while (i <= 10) {
+//		if (i == 5)
+//			continue;
+//
+//		printf("%d ", i);
+//
+//		i++;
+//
+//	}
+//	/*while (i <= 10) {
+//		if (i == 5) {
+//			break;
+//		}
+//
 //		printf("%d ", i);
 //		i++;
-//	}
+//	}*/
 //	return 0;
 //}
+
+//getchar
+
+//EOF - end of file - 文件结束标志
+int main() {
+	int ch = 0;
+	//ctrl + z - getchar读取结束
+	while ((ch = getchar()) != EOF) {
+
+		putchar(ch);
+
+	}
+}
