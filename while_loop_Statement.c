151cd314c06117bf865e661214ef50b19feb099b
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-02-16 21:18:13 +0800

    Update while_loop_Statement.c

--- while_loop_Statement.c
+++ while_loop_Statement.c
@@ -42,12 +42,46 @@
 //getchar
 
 //EOF - end of file - 文件结束标志
+//int main() {
+//	int ch = 0;
+//	//ctrl + z - getchar读取结束
+//	while ((ch = getchar()) != EOF) {
+//
+//		putchar(ch);
+//
+//	}
+//}
+
+
+//int main() {
+//	char password[20] = { 0 };
+//	
+//	printf("plz kaybord password\n");
+//
+//	scanf("%s", password);
+//	//清理缓冲区
+//	//getchar(); //处理 '\n'
+//	//清理缓冲区中的多个字符
+//	int tmp;
+//	while ((tmp = getchar()) != '\n') {
+//
+//	}
+//	printf("plz check ur password(Y/N)\n");
+//	int ch = getchar();
+//	if (ch == 'Y') {
+//		printf("check in");
+//	}else{
+//		printf("error\n");
+//	}
+//	return 0;
+//}
+
 int main() {
 	int ch = 0;
-	//ctrl + z - getchar读取结束
 	while ((ch = getchar()) != EOF) {
-
+		if (ch < '0' || ch>'9')
+			continue;
 		putchar(ch);
-
 	}
+	return 0;
 }
