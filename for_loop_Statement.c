39696a3051b24ba7aae094db68153ab084565335
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-02-17 22:06:08 +0800

    Create for_loop_Statement.c

---
+++ for_loop_Statement.c
@@ -0,0 +1,35 @@
+//int main() {
+//	for (int i = 1; i <= 10; i++){
+//
+//		/*if (i == 5)
+//			break;*/
+//		if (i == 5) {
+//			continue;
+//		}
+//
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//循环体内改变循环变量的例子
+//int main() {
+//	int i = 0;
+//	for (i = 1; i <= 10; i++) {
+//		printf("%d ", i);
+//
+//		for (int j = 0; j < 10; i++) {
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int mian() {
+//	//判断部分的省略 - 判断部分恒为真
+//	for (;;) {
+//		printf("hehe\n");
+//	}
+//	return 0;
+//}
