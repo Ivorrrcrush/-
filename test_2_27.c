aa9ddabc5c7be08826dca34c5c325ad8c12d7f05
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-02-27 21:18:51 +0800

    Create test_2_27.c

---
+++ test_2_27.c
@@ -0,0 +1,21 @@
+int main() {
+	int a, b,c,d;
+	scanf("%d%d", &a, &b);
+	int i = 0;
+	if (a < b) {
+		c = a;
+	}
+	else {
+		c = b;
+	}
+	for (i = c;i>=0; i--) {
+		if (a % i == 0 && b % i == 0) {
+			printf("%d\n", i);
+			break;
+		}
+	}
+
+
+	return 0;
+}
+//苛待补充....
