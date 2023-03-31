0d722aa2c086208f55340d4ce8664e8a90353ca5
Ivorrrcrush <123283775+Ivorrrcrush@users.noreply.github.com>
2023-03-31 17:31:47 +0800

    Create test_3_31.c

---
+++ test_3_31.c
@@ -0,0 +1,35 @@
+#include <string.h>
+int search(int arr[],int a,int sz) {
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right) {
+		int mid = (left + right) / 2;
+		if (arr[left] < a) {
+			left = mid;
+		}
+		else if (arr[right] > a) {
+			right = mid;
+		}
+		else {
+			return mid;
+		}
+	}
+	return -1;
+}
+
+int main() {
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	int key = 7;
+
+	int sz = sizeof(arr) / sizeof(arr[0]);
+
+	int x = search(arr, key,sz);
+	if (-1 == x) {
+		printf("没找到\n");
+	}
+	else {
+		printf("找到了,下标是：%d ",x );
+	}
+	return 0;
+}
